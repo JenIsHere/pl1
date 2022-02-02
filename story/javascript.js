0eb2c5f8b7dd30e8e1ab5166a821d691349fb6b5
Jenish Shah <91283665+JenIsHere@users.noreply.github.com>
2022-02-02 17:46:26 -0500

    Update javascript.js

--- story/javascript.js
+++ story/javascript.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 31.7860603, lng: -132.0853276 },
-    zoom: 10,
+    center: { lat: 40.684128, lng: -102.028191},
+    zoom: 5,
   });
 }
